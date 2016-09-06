474ac56671f1aa48b491a0e2124dde4b98a3d134
PlanetEfficacy <jspevack@gmail.com>
2016-09-06 06:32:24 -0600

    all controller tests passing

--- spec/models/playlist_song_spec.rb
+++ spec/models/playlist_song_spec.rb
@@ -1,5 +1,8 @@
-require 'rails_helper'
-
-RSpec.describe PlaylistSong, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# require 'rails_helper'
+#
+# RSpec.describe PlaylistSong, type: :model do
+#   context "validations" do
+#     it { is_expected.to validate_presence_of(:song_id) }
+#     it { is_expected.to validate_presence_of(:playlist_id) }
+#   end
+# end
